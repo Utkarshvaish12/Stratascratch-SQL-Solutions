b646577bc90b6286e468188c95ea2845ce928153
Utkarsh Vaish <122693972+Utkarshvaish12@users.noreply.github.com>
2023-10-02 00:05:52 +0530

    Add files via upload

---
+++ Medium/Users By Average Session Time.sql
@@ -0,0 +1,12 @@
+SELECT user_id, AVG(TIMESTAMPDIFF(SECOND, load_time, exit_time)) AS session_time
+FROM (
+    SELECT 
+        DATE(timestamp), 
+        user_id, 
+        MAX(IF(action = 'page_load', timestamp, NULL)) as load_time,
+        MIN(IF(action = 'page_exit', timestamp, NULL)) as exit_time
+    FROM facebook_web_log
+    GROUP BY 1, 2
+) as t 
+GROUP BY user_id
+HAVING session_time IS NOT NULL;
